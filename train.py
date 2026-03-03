709f6a347ec01108cf9e7e41d921ea2e20b0b9bd
Waseem Almazrua <waseeme900@gmail.com>
2026-03-03 17:45:47 +0300

    change file the path for data

--- train.py
+++ train.py
@@ -58,7 +58,7 @@ logger = logging.getLogger(__name__)
 # =====================================================
 
 DATA_DIR        = Path(os.environ.get("DATA_ROOT",
-                        "/data/brats/BraTS2020_TrainingData"))
+                        "/workspace/data/BraTS20"))
 CHECKPOINT_DIR  = Path(os.environ.get("CHECKPOINT_DIR", "./checkpoints"))
 
 MAX_EPOCHS      = int(os.environ.get("MAX_EPOCHS",    "100"))
